ffdb91c92951e7799183f9d918951a705ef36c9c
cosmicorganism <cosmicorganism@gmail.com>
2013-11-18 18:36:06 -0800

    initial tests

--- test/pcap_tests.cpp
+++ test/pcap_tests.cpp
@@ -1,11 +1,21 @@
 #include "gtest/gtest.h"
 #include <pcap_cpp/pcap.hpp>
-
 namespace pcap_tests {
 
-TEST(pcap_tests, create_test) {
-  auto device = libpcap::create("en0");
-  EXPECT_TRUE(device);
+TEST(pcap_tests, get_default) {
+  auto device = libpcap::find_default_device_name();
+  std::cout << "Default device found: " << device << "\n";
+  EXPECT_FALSE(device.empty());
 }
+
+TEST(pcap_tests, find_all_devices_test) {
+  auto devices = libpcap::find_all_devices();
+  std::cout << "All devices:\n";
+  for (const auto &device : devices) {
+    std::cout << device << "\n"; 
+  }
+  EXPECT_FALSE(devices.empty());  
+}
+
 
 } //namespace test_vector
